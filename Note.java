4f266cde43b364abb1d965c54a761165cde2c3ca
Leo Saunier <leo.saunier@etu.univ-orleans.fr>
2024-02-22 12:22:37 +0100

    intégration coef à Note + Etudiant et ses getteur

--- Note.java
+++ Note.java
@@ -1,15 +1,17 @@
 public class Note {
     
     private double note;
+    private int coef;
     private String nomCotrole;
     private Matiere matiere;
     private Etudiant etudiant;
 
-    public Note(double note, String nomControle, Matiere matiere, Etudiant etudiant){
+    public Note(double note, int coef, String nomControle, Matiere matiere, Etudiant etudiant){
         this.note=note;
         this.nomCotrole=nomControle;
         this.matiere=matiere;
         this.etudiant=etudiant;
+        this.coef=coef;
     }
 
     public double getNote(){
@@ -26,6 +28,10 @@ public class Note {
 
     public String getNomEtudiant(){
         return this.etudiant.getNom();
+    }
+
+    public int getCoef() {
+        return coef;
     }
 
     public void changerNote(double valeur){
