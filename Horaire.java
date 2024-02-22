1efd07f88a24bff0758c3d7f7a3779d6266052ec
Toma1345 <thomas.brossier@etu.univ-orleans.fr>
2024-02-22 11:29:59 +0100

    Modification dans les fichiers : User.java, Horaire.java, Salle.java

--- Horaire.java
+++ Horaire.java
@@ -3,10 +3,12 @@ import java.time.LocalDateTime;
 public class Horaire {
     private LocalDateTime startDate;
     private LocalDateTime endDate;
+    private Salle salle;
 
-    public Horaire(LocalDateTime start, LocalDateTime end){
+    public Horaire(LocalDateTime start, LocalDateTime end, Salle salle){
         this.startDate = start;
         this.endDate = end;
+        this.salle = salle;
     }
 
     public LocalDateTime getStartDate(){
@@ -15,5 +17,9 @@ public class Horaire {
 
     public LocalDateTime getEndDate(){
         return this.endDate;
+    }
+
+    public Salle getSalle(){
+        return this.salle;
     }
 }
--- Salle.java
+++ Salle.java
@@ -1,4 +1,11 @@
 public class Salle {
     private String name;
-    
+
+    public Salle(String name){
+        this.name = name;
+    }
+
+    public String getSalle(){
+        return this.name;
+    }
 }
