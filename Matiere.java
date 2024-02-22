3fe91ef7c6d7caaf8d27f8c53d9c02a1e9becedf
Leo Saunier <leo.saunier@etu.univ-orleans.fr>
2024-02-22 11:41:39 +0100

    ajouterNote + ajout des notes dans la matière

--- Matiere.java
+++ Matiere.java
@@ -1,15 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public enum Matiere{
     MATH("Maths"), INITPROG("Initiation à la programmation"), 
     ANGLAIS("Anglais"), DEVWEB("Développement Web"), 
     BDD("Base de Donnée"), BASNIVEAU("Développement bas niveau"), POO("Programmation orienté objet");
 
-    private final String nom;
+    private List<Note> notes;
+    private String nom;
 
     private Matiere(String nom){
+        this.notes = new ArrayList<>();
         this.nom=nom;
     }
 
     public String getNom(){
         return this.nom;
+    }
+
+    public void ajouterNote(Note note){
+        this.notes.add(note);
     }
 }
