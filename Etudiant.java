447d1a4d96b15bf643de3494be95a4afa4ff3bc6
Leo-sau <143810312+Leo-sau@users.noreply.github.com>
2024-02-22 12:28:35 +0100

    Leo (#4)
    * création branch leo
    
    * Matire.java <enum>
    
    * ajouterNote + ajout des notes dans la matière
    
    * Création class Note
    
    * getter + changerNote
    
    * intégration coef à Note + Etudiant et ses getteur

---
+++ Etudiant.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Etudiant {
+    private String prenom;
+    private String nom;
+    private List<Note> notes;
+    private int age;
+    private List<Groupe> groupes;
+
+    public Etudiant(String prenom, String nom, int age){
+        this.prenom=prenom;
+        this.nom=nom;
+        this.age=age;
+        this.notes= new ArrayList<>();
+        this.groupes= new ArrayList<>();
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public List<Groupe> getGroupes() {
+        return groupes;
+    }
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    public List<Note> getNotes(Matiere matiere){
+        List<Note> notes = new ArrayList<>();
+        for (Note note : this.notes){
+            if (note.getNomMatiere()==matiere.getNom()){
+                notes.add(note);
+            }
+        }
+        return notes;
+    }
+
+    public 
+}
